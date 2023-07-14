527df6334ee01ec139689c3d1d4eb8866167b773
LucasHenriqueLS <lucashlsl100@gmail.com>
2023-07-14 20:38:31 -0300

    Messages system implemented

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Moment } from 'src/app/Moment';
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -10,7 +12,11 @@ import { MomentService } from 'src/app/services/moment.service';
 export class NewMomentComponent {
   btnText: string = 'Compartilhar!';
 
-  constructor(private momentService: MomentService) {}
+  constructor(
+    private momentService: MomentService,
+    private messageService: MessagesService,
+    private router: Router  
+  ) {}
 
   async createHandler(moment: Moment) {
     const formData = new FormData();
@@ -23,5 +29,9 @@ export class NewMomentComponent {
     }
 
     await this.momentService.createMoment(formData).subscribe();
+
+    this.messageService.add('Momento adicionado com sucesso!');
+
+    this.router.navigate(['/']);
   }
 }
